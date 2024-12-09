86f0ac609f734164c248d13a78535ad666779370
antoniom <antonio.mavrin@prehnit.hr>
2024-12-09 16:58:42 +0100

    + added open account functionality

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
+import { useReducer } from "react";
 import "./index.css";
 
-/*
-INSTRUCTIONS / CONSIDERATIONS:
-
-1. Let's implement a simple bank account! It's similar to the example that I used as an analogy to explain how useReducer works, but it's simplified (we're not using account numbers here)
-
-2. Use a reducer to model the following state transitions: openAccount, deposit, withdraw, requestLoan, payLoan, closeAccount. Use the `initialState` below to get started.
-
-3. All operations (expect for opening account) can only be performed if isActive is true. If it's not, just return the original state object. You can check this right at the beginning of the reducer
-
-4. When the account is opened, isActive is set to true. There is also a minimum deposit amount of 500 to open an account (which means that the balance will start at 500)
-
-5. Customer can only request a loan if there is no loan yet. If that condition is met, the requested amount will be registered in the 'loan' state, and it will be added to the balance. If the condition is not met, just return the current state
-
-6. When the customer pays the loan, the opposite happens: the money is taken from the balance, and the 'loan' will get back to 0. This can lead to negative balances, but that's no problem, because the customer can't close their account now (see next point)
-
-7. Customer can only close an account if there is no loan, AND if the balance is zero. If this condition is not met, just return the state. If the condition is met, the account is deactivated and all money is withdrawn. The account basically gets back to the initial state
+/* 
+INSTRUCTIONS
+All operations (expect for opening account) can only be performed if isActive is true. If it's not, just return the original state object. 
+You can check this right at the beginning of the reducer. When the account is opened, isActive is set to true. 
+There is also a minimum deposit amount of 500 to open an account (which means that the balance will start at 500). 
+Customer can only request a loan if there is no loan yet. 
+If that condition is met, the requested amount will be registered in the 'loan' state, and it will be added to the balance. 
+If the condition is not met, just return the current state. 
+When the customer pays the loan, the opposite happens: the money is taken from the balance, and the 'loan' will get back to 0. 
+This can lead to negative balances, but that's no problem, because the customer can't close their account now (see next point). 
+Customer can only close an account if there is no loan, AND if the balance is zero. 
+If this condition is not met, just return the state. 
+If the condition is met, the account is deactivated and all money is withdrawn. The account basically gets back to the initial state. 
 */
 
 const initialState = {
@@ -24,15 +22,58 @@ const initialState = {
   isActive: false,
 };
 
+function reducer(state, action) {
+  switch (action.type) {
+    case "openAccount":
+      return {
+        ...state,
+        balance: 500,
+        isActive: true,
+      };
+    case "deposit":
+      if (state.isActive) {
+      } else {
+        return state;
+      }
+    case "withdraw":
+      if (state.isActive) {
+      } else {
+        return state;
+      }
+    case "requestLoan":
+      if (state.isActive) {
+      } else {
+        return state;
+      }
+    case "payLoan":
+      if (state.isActive) {
+      } else {
+        return state;
+      }
+    case "closeAccount":
+      if (state.isActive) {
+      } else {
+        return state;
+      }
+    default:
+      throw new Error("Unknown action");
+  }
+}
+
 export default function App() {
+  const [{ balance, loan, isActive }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+
   return (
     <div className="App">
       <h1>useReducer Bank Account</h1>
-      <p>Balance: X</p>
-      <p>Loan: X</p>
+      <p>Balance: {balance}</p>
+      <p>Loan: {loan}</p>
 
       <p>
-        <button onClick={() => {}} disabled={false}>
+        <button onClick={() => dispatch({ type: "openAccount" })}>
           Open account
         </button>
       </p>
